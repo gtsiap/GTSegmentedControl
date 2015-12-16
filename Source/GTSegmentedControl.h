3a13dfdb5e93619bea6e447961ce388818c714d8
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-12-16 14:20:19 +0200

    Initial commit

---
+++ Source/GTSegmentedControl.h
@@ -0,0 +1,15 @@
+//
+//  GTSegmentedControl.h
+//  GTSegmentedControl
+//
+//  Created by Giorgos Tsiapaliokas on 16/12/15.
+//  Copyright © 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTSegmentedControl.
+FOUNDATION_EXPORT double GTSegmentedControlVersionNumber;
+
+//! Project version string for GTSegmentedControl.
+FOUNDATION_EXPORT const unsigned char GTSegmentedControlVersionString[];
